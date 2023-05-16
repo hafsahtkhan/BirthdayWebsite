c161dccbbfeccf23a45d23b82a5fe0aee4914772
Hafsah Khan <hafsahtkhan@gmail.com>
2023-05-16 16:05:20 -0500

    here

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
-const card = document.querySelector('.card_inner');
+const card = document.querySelector('.card_inner1');
 
 card.addEventListener('click', function() {
     card.classList.toggle('is-flipped');
+})
+
+const card2 = document.querySelector('.card_inner2');
+
+card2.addEventListener('click', function() {
+    card2.classList.toggle('is-flipped');
+})
+
+
+const card3 = document.querySelector('.card_inner3');
+
+card3.addEventListener('click', function() {
+    card3.classList.toggle('is-flipped');
 })
